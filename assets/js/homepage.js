43d96831dba2c2623242c59c8c190a9a8624f0fd
Ricky Sullivan <rickyplaysdrums@gmail.com>
2020-10-28 00:42:11 -0500

    first issue

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,11 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var displayRepos = function(repos, searchTerm){
+//check if the API returned any repos
+    if (repos.length === 0){
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     //clear old content
@@ -54,10 +59,19 @@ var getUserRepos = function(user) {
 //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 //make a request to the url
-    fetch(apiUrl).then(function(response){
+    fetch(apiUrl)
+    .then(function(response){
+        if (response.ok){
         response.json().then(function(data){   
             displayRepos(data, user);
         });
+        }else {
+            alert("ERROR: " + response.statusText);
+        }
+    })
+    .catch(function(error){
+//Notice this `catch()` getting chained onto the end of the `.then()`
+        alert("UNABLE TO CONNECT TO GITHUB!!!");
     });
     
 };
